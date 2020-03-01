4b49e882c93879c5bfb8fd3a52b467f939cf9321
drmeloy <drmeloy@gmail.com>
2020-02-29 17:54:38 -0800

    cleaned up

--- index.js
+++
@@ -1,9 +0,0 @@
-import displayAbout from './utils/display-about.js';
-
-const aboutButton = document.getElementById('about');
-const projectsButton = document.getElementById('projects');
-const contactButton = document.getElementById('contact');
-
-aboutButton.addEventListener('click', displayAbout);
-projectsButton.addEventListener('click', displayProjects);
-contactButton.addEventListener('click', displayContact);
